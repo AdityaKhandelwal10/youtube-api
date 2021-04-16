c25b302459868c1b563d23a6c56886af88a9b071
Aditya Khandelwal <akaditya05@gmail.com>
2021-04-16 20:20:50 +0530

    Setting up Celery -Done

---
+++ youtube_api/celery.py
@@ -0,0 +1,17 @@
+#Defines the celery instance for the app
+
+from __future__ import absolute_import
+import os
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','youtube_api.settings')
+
+app = Celery('youtube_api')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
